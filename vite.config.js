26f244009212133d51cc1c0e72146d337b6a20f3
acippicacipa <marketing.studio16@gmail.com>
2025-07-28 14:42:21 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://acippicacipa.pythonanywhere.com',
         changeOrigin: true,
         secure: false,
       },
